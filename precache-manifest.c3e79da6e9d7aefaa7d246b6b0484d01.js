dbf9fc2927c876da2a7223187cba8bce7bc8012d
maximborodkin <maxim.borodkin1999@yandex.ru>
2020-09-14 19:56:34 +0300

    Updates

---
+++ precache-manifest.c3e79da6e9d7aefaa7d246b6b0484d01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b3b60132060499c00d71fd6532a17e8",
+    "url": "/vk-goodcode/index.html"
+  },
+  {
+    "revision": "b2d29db14c7b0e8b126c",
+    "url": "/vk-goodcode/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "b2d29db14c7b0e8b126c",
+    "url": "/vk-goodcode/static/js/2.b5ac53b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vk-goodcode/static/js/2.b5ac53b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af64f52d13ab00fb4ecd",
+    "url": "/vk-goodcode/static/js/main.2818d9e5.chunk.js"
+  },
+  {
+    "revision": "5377b232b7b4a885aa30",
+    "url": "/vk-goodcode/static/js/runtime-main.a679e6ac.js"
+  }
+]);
