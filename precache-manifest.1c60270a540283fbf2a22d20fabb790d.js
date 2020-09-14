d849d483a402cc575d635338518bad83dc8d0628
maximborodkin <maxim.borodkin1999@yandex.ru>
2020-09-14 22:29:58 +0300

    Updates

--- precache-manifest.1c60270a540283fbf2a22d20fabb790d.js
+++ precache-manifest.1c60270a540283fbf2a22d20fabb790d.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "01f8cb2938623e8c37b9dd3068d2f105",
+    "revision": "a8f48bbd4d1171c32e648e613d466415",
     "url": "/vk-goodcode/index.html"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/vk-goodcode/static/js/2.b5ac53b3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "90f6cf3bc0a11b8abe0b",
-    "url": "/vk-goodcode/static/js/main.ed4a522b.chunk.js"
+    "revision": "9a7505c9c69464841d34",
+    "url": "/vk-goodcode/static/js/main.ba751777.chunk.js"
   },
   {
     "revision": "5377b232b7b4a885aa30",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vk-goodcode/precache-manifest.61d7fb3457f3d10de6d2d01fa8af3042.js"
+  "/vk-goodcode/precache-manifest.1c60270a540283fbf2a22d20fabb790d.js"
 );
 
 self.addEventListener('message', (event) => {
